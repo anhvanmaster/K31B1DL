9c96e993ba6f64d33506dd865d690fbd9308b021
anhvanmaster <anhvanmaster@gmail.com>
2017-09-28 21:38:20 +0700

    code đi học
    học ở trường up lên

---
+++ Lesson13/public/js/main.js
@@ -0,0 +1,42 @@
+// var name = 'Văn';
+// 	age = 18;
+// 	homeTown = 'Phú Thọ';
+// // alert(`ho ten: ${name} \ntuoi: ${age} \ndia chi: ${homeTown}`);
+// // toán tử =
+// var x = 100;
+// console.log(x);
+// // x = 'mot tram';
+// // console.log(x);
+// //phep cong
+// var y = x+1;
+// console.log(y);
+// var z = "100" - "1";
+// console.log(z);
+// console.log(10 % 3);
+// console.log(Math.sqrt(4));
+// console.log(Math.pow(4));
+// console.log()
+// var a= 4;
+// var b= 8;
+// var c= 2;
+// var delta = Math.pow(b,2)-4*a*c;
+// console.log(delta);
+// var a = 3;
+// var b = 4;
+// var c = 5;
+// var p = (a+b+c)/2;
+// console.log('chu vi: ' + p*2);
+// console.log(
+// 	'diện tích: ' + Math.sqrt( p * (p-a) * (p-b) * (p-c) )
+// );
+var a= 8;
+var b= 4;
+var c= 5;
+var max = a;
+if (max <= b) {
+	max=b;
+}
+if (max <= c) {
+	max=c;
+}
+console.log(max);
