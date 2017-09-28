0b85f2d5d0ebe5e59231b051efdfc0c4aba47256
anhvanmaster <anhvanmaster@gmail.com>
2017-09-28 23:01:06 +0700

    Bài tập về nhà

---
+++ Lesson14/public/js/main.js
@@ -0,0 +1,35 @@
+// Bài 1:
+alert(`Nhập một số và tìm bội số của nó trong khoảng từ 1-15.`);
+var a = prompt('Nhập số a:');
+a = parseInt(a);
+document.writeln(`Bài 1:<br> Bội số của a trong khoảng từ 1-15 là: `)
+for (var i = 1; i < 15; i++) {
+	if (i%a==0) {
+		document.write(`${i}, `);
+	}
+}
+document.write('<br>');
+// Bài 2
+alert(`Nhập câu a và dòng kí tự b. xét xem trong a chứa bao nhiêu kí tự của b.`);
+var a = prompt(`Nhập chuỗi a:`,"");
+var b = prompt(`Nhập dòng kí tự b:`,'');
+var d = 0;
+for (var i = 0; i < b.length; i++) {
+	for (var j = 0; j < a.length; j++) {
+		if (b[i]===a[j]) {
+				++d;
+				break;
+		}
+	}
+}
+document.write(`Bài 2:<br> Có ${d} kí tự của b nằm trong a`);
+document.write('<br>');
+// Bài 3
+alert(`Hiện ra bảng cửu chương`);
+document.write(`Bài 3:<br>`)
+for (var i = 2; i <= 9; i++) {
+	for (var j = 2; j <= 9; j++) {
+		document.write(`${i} x ${j} = ${i*j} <br>`);
+	}
+	document.write('<br>');
+}
