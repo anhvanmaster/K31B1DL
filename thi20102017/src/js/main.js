7fd985dea89b8fc447f4938869f1e6ca350dee61
anhvanmaster <anhvanmaster@gmail.com>
2017-11-10 19:11:51 +0700

    update

---
+++ thi20102017/src/js/main.js
@@ -0,0 +1,13 @@
+var sectionFly = document.createElement("section");
+sectionFly.className = "fly";
+var imgFly = document.createElement("img");
+var imgArr = [
+	"src/img/img-h1.png",
+	"src/img/img-h2.png",
+	"src/img/img-h3.png",
+	"src/img/img-h4.png",
+	"src/img/img-f1.png",
+	"src/img/img-f2.png",
+	"src/img/img-f3.png"
+];
+sectionFly.appendChild(imgFly);
